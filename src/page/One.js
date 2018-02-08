5c0a0c1f9561b3929b748c821e4c61d86ccff79a
zard <zardizumi941116@163.com>
2018-02-08 11:45:02 +0800

    2.8 11:44

--- src/page/One.js
+++ src/page/One.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class One extends React.Component {
     render(){
         return (
             <div id='one'>
                <h1>123</h1>
+               <MuiThemeProvider>
+                <FlatButton label="Default" />
+                </MuiThemeProvider >
             </div>
         )
     }
