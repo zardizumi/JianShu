04c1f9c3d1933e31af8f6452fe6ae6f08df2f4cc
zard <zardizumi941116@163.com>
2018-02-12 21:11:11 +0800

    1.1

--- src/page/TopNav.js
+++ src/page/TopNav.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import $ from 'jquery'
 import '../css/topNav.css'
+import '../../node_modules/modern-normalize/modern-normalize.css'
+import '../css/font-awesome.min.css'
 import Logo from '../images/logo.png'
+
+import _data from '../data/d.json'
 class TopNav extends React.Component {
     constructor(props){
         super(props);
         this.state  = {
-            logoSrc:'https://cdn2.jianshu.io/assets/web/nav-logo-4c7bbafe27adc892f3046e6978459bac.png'
         }
+       this.inputFocus = this.inputFocus.bind(this);
+       this.inputBlur = this.inputBlur.bind(this);
+    }
+    componentDidMount(){
+        console.log(_data);
+    }
+    inputFocus(){
+        $('.topNav_input').animate({width:"200px"},400)
+    }
+    inputBlur(){
+        $('.topNav_input').animate({width:"130px"},400);
+        console.log(this);
     }
     render(){
         return (
             <div id='topnav'>
-                <a href="https://www.jianshu.com/"  class='logo'>
+                <a href="https://www.jianshu.com/"  className='logo'>
                     <img src={Logo} style={{height:"100%"}} alt="123"/>
                 </a>
-                <NavLink to='/'>首页</NavLink>
-                <NavLink to='/one'>下载App</NavLink>
-                <NavLink to='/two'>登陆</NavLink>
+                <NavLink to='/' className='link link_active'>首页</NavLink>
+                <NavLink to='/one' className='link'>下载App</NavLink>
+                <div className='topNav_input'>
+                    <input type="text" placeholder='搜索' onClick={this.inputFocus} onBlur={this.inputBlur}/>
+                    <i className="fa fa-search " onClick={this.click}></i>
+                </div>
+                <NavLink to='/two' className='link' style={{float:"right"}}>登陆</NavLink>
+                
             </div>
         )
     }
